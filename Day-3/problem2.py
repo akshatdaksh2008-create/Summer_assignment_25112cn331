1d2553a69c2c7d00c134928bf05ff12a109f3cf7
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-03 21:06:36 +0530

    Update problem2.py

--- Day-3/problem2.py
+++ Day-3/problem2.py
@@ -1,1 +1,9 @@
-
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+for num in range(start, end + 1):
+    count = 0
+  for i in range(1, num + 1):
+        if num % i == 0:
+            count = count + 1
+  if count == 2:
+        print(num)

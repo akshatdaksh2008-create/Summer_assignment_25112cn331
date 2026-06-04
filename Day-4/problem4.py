f90d0ee6da9227c2d4f1379453f9965e3cfa4aa6
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-04 13:37:32 +0530

    Update problem4.py

--- Day-4/problem4.py
+++ Day-4/problem4.py
@@ -1,1 +1,12 @@
-
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+print("Armstrong numbers are:")
+for num in range(start, end + 1):
+    temp = num
+    sum = 0
+    while temp > 0:
+        digit = temp % 10
+        sum = sum + digit ** 3
+        temp = temp // 10
+    if sum == num:
+        print(num)

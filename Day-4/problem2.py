ee26e5f6c33ccfc6746f2a0de07d2c2c0c288ef1
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-04 13:35:09 +0530

    Update problem2.py

--- Day-4/problem2.py
+++ Day-4/problem2.py
@@ -1,1 +1,13 @@
-
+n = int(input("Enter the value of n: "))
+a = 0
+b = 1
+if n == 1:
+    print("Nth Fibonacci term is:", a)
+elif n == 2:
+    print("Nth Fibonacci term is:", b)
+else:
+    for i in range(3, n + 1):
+        c = a + b
+        a = b
+        b = c
+    print("Nth Fibonacci term is:", b)

30355d250fa116df59b62069d5802f9fede7b0d9
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-04 13:44:14 +0530

    Update problem4.py

--- Day-5/problem4.py
+++ Day-5/problem4.py
@@ -1,1 +1,12 @@
-
+num = int(input("Enter a number: "))
+largest = 1
+for i in range(2, num + 1):
+    if num % i == 0:
+        prime = True
+       for j in range(2, i):
+            if i % j == 0:
+                prime = False
+                break
+        if prime:
+            largest = i
+print("Largest Prime Factor =", largest)

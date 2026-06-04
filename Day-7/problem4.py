58888136421b0e940f992aca77d9dbcbfd21e3e1
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-04 13:56:50 +0530

    Update problem4.py

--- Day-7/problem4.py
+++ Day-7/problem4.py
@@ -1,1 +1,9 @@
-
+def reverse_num(n, rev):
+    if n == 0:
+        return rev
+    else:
+        rev = rev * 10 + (n % 10)
+        return reverse_num(n // 10, rev)
+num = int(input("Enter a number: "))
+result = reverse_num(num, 0)
+print("Reversed number =", result)

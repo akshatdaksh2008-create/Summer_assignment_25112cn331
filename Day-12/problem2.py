a99006c6a4d8be7e9544ecc74ac4ab5b6bc2f25e
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-05 17:39:34 +0530

    Update problem2.py

--- Day-12/problem2.py
+++ Day-12/problem2.py
@@ -1,1 +1,13 @@
-
+def check_armstrong(n):
+    s = 0
+    temp = n
+    while temp > 0:
+        d = temp % 10
+        s = s + d ** len(str(n))
+        temp = temp // 10
+    return s == n
+num = int(input("Enter a number: "))
+if check_armstrong(num):
+    print("Armstrong")
+else:
+    print("Not Armstrong")

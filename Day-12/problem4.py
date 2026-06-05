f3c0fa7023862de2f6d71da87d5d1201e4b48968
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-05 17:44:00 +0530

    Update problem4.py

--- Day-12/problem4.py
+++ Day-12/problem4.py
@@ -1,1 +1,11 @@
-
+def check_perfect(n):
+    total = 0
+    for i in range(1, n):
+        if n % i == 0:
+            total += i
+    return total
+num = int(input("Enter a number: "))
+if check_perfect(num) == num:
+    print("Perfect Number")
+else:
+    print("Not Perfect Number")

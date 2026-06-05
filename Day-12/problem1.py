45aeb08d14c7d4ea2c915a95b2218382c0d06800
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-05 17:37:52 +0530

    Update problem1.py

--- Day-12/problem1.py
+++ Day-12/problem1.py
@@ -1,1 +1,16 @@
-
+def palindrome(n):
+    temp = n
+    rev = 0
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n = n // 10
+    if temp == rev:
+        return True
+    else:
+        return False
+num = int(input("Enter a number: "))
+if palindrome(num):
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")

85b930625079865edb312714d4ca6c1526b853bb
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-05 17:41:43 +0530

    Update problem3.py

--- Day-12/problem3.py
+++ Day-12/problem3.py
@@ -1,1 +1,10 @@
-
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        print(a, end=" ")
+        c = a + b
+        a = b
+        b = c
+num = int(input("Enter number of terms: "))
+fibonacci(num)

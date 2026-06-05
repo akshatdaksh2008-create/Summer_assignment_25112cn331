36d0cbfea6542503199a3da08bf47e422a02e8d5
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-05 17:32:34 +0530

    Update problem3.py

--- Day-11/problem3.py
+++ Day-11/problem3.py
@@ -1,1 +1,10 @@
-
+def check_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+num = int(input("Enter a number: "))
+if num > 1 and check_prime(num):
+    print("Prime")
+else:
+    print("Not Prime")

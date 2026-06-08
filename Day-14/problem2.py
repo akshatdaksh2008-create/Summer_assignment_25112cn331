8dc99e09b545ead8865a3d2be6827aab77d819d5
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:18:11 +0530

    Update problem2.py

--- Day-14/problem2.py
+++ Day-14/problem2.py
@@ -1,1 +1,11 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+x = int(input("Enter element to find frequency: "))
+count = 0
+for i in arr:
+    if i == x:
+        count += 1
+print("Frequency of", x, "=", count)

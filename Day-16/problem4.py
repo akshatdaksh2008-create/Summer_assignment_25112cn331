34d72603b8e58f0806c6a80cea8b952f425aca2f
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:40:26 +0530

    Update problem4.py

--- Day-16/problem4.py
+++ Day-16/problem4.py
@@ -1,1 +1,11 @@
-
+arr = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+new_arr = []
+for i in arr:
+    if i not in new_arr:
+        new_arr.append(i)
+print("Array after removing duplicates:")
+print(new_arr)

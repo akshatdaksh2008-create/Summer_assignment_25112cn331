9afdbbc9161ae7f02b1fc780ea80b214b9fab0ff
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:39:27 +0530

    Update problem3.py

--- Day-16/problem3.py
+++ Day-16/problem3.py
@@ -1,1 +1,14 @@
-
+arr = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+target = int(input("Enter the required sum: "))
+found = False
+for i in range(n):
+    for j in range(i + 1, n):
+        if arr[i] + arr[j] == target:
+            print("Pair found:", arr[i], "and", arr[j])
+            found = True
+if found == False:
+    print("No pair found")

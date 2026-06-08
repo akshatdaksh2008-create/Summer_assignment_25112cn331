311fbe4af8ae133ebce9b5fa6af4bba8ec8693e2
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:50:36 +0530

    Update problem4.py

--- Day-18/problem4.py
+++ Day-18/problem4.py
@@ -1,1 +1,13 @@
-
+arr = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+for i in range(n):
+    for j in range(i + 1, n):
+        if arr[i] < arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+print("Array in descending order:")
+print(arr)

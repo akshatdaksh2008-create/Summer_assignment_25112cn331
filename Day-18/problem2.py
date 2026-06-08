23226ebab2328246643addccaed4fc8ff435d739
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:47:48 +0530

    Update problem2.py

--- Day-18/problem2.py
+++ Day-18/problem2.py
@@ -1,1 +1,15 @@
-
+arr = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+for i in range(n):
+    min_index = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    temp = arr[i]
+    arr[i] = arr[min_index]
+    arr[min_index] = temp
+print("Sorted array:")
+print(arr)

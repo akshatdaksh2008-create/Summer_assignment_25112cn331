89a94694ccd5fa1d36c3065b374116e1fc8bb75c
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:27:57 +0530

    Update problem4.py

--- Day-15/problem4.py
+++ Day-15/problem4.py
@@ -1,1 +1,15 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+result = []
+for i in arr:
+    if i != 0:
+        result.append(i)
+zero_count = n - len(result)
+for i in range(zero_count):
+    result.append(0)
+print("Array after moving zeroes to end:")
+for i in result:
+    print(i, end=" ")

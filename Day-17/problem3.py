bbc097726555d489fc6ec7a989153e7a936db87c
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:44:42 +0530

    Update problem3.py

--- Day-17/problem3.py
+++ Day-17/problem3.py
@@ -1,1 +1,16 @@
-
+arr1 = []
+arr2 = []
+n1 = int(input("Enter number of elements in first array: "))
+for i in range(n1):
+    num = int(input("Enter element: "))
+    arr1.append(num)
+n2 = int(input("Enter number of elements in second array: "))
+for i in range(n2):
+    num = int(input("Enter element: "))
+    arr2.append(num)
+intersection = []
+for i in arr1:
+    if i in arr2 and i not in intersection:
+        intersection.append(i)
+print("Intersection of arrays:")
+print(intersection)

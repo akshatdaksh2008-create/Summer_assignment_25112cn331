730644f8a392bb43f8f257da0df6255aab207ee6
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:12:40 +0530

    Update problem4.py

--- Day-13/problem4.py
+++ Day-13/problem4.py
@@ -1,1 +1,14 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+even = 0
+odd = 0
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("Number of even elements =", even)
+print("Number of odd elements =", odd)

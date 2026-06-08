d7180fe6650d87aea7303afc1773d0d3a9f73cbf
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:26:39 +0530

    Update problem3.py

--- Day-15/problem3.py
+++ Day-15/problem3.py
@@ -1,1 +1,12 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+last = arr[n - 1]
+for i in range(n - 1, 0, -1):
+    arr[i] = arr[i - 1]
+arr[0] = last
+print("Array after right rotation:")
+for i in arr:
+    print(i, end=" ")

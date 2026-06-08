ead6138ec1f210fccb4d81442527bd7dce7fd0b5
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:43:38 +0530

    Update problem2.py

--- Day-17/problem2.py
+++ Day-17/problem2.py
@@ -1,1 +1,19 @@
-
+arr1 = []
+arr2 = []
+n1 = int(input("Enter number of elements in first array: "))
+for i in range(n1):
+    num = int(input("Enter element: "))
+    arr1.append(num)
+n2 = int(input("Enter number of elements in second array: "))
+for i in range(n2):
+    num = int(input("Enter element: "))
+    arr2.append(num)
+union = []
+for i in arr1:
+    if i not in union:
+        union.append(i)
+for i in arr2:
+    if i not in union:
+        union.append(i)
+print("Union of arrays:")
+print(union)

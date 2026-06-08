151aeae6ef71423c69034a4adafa7a1095922fa6
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:09:41 +0530

    Update problem2.py

--- Day-13/problem2.py
+++ Day-13/problem2.py
@@ -1,1 +1,10 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+sum = 0
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+    sum = sum + num
+average = sum / n
+print("Sum =", sum)
+print("Average =", average)

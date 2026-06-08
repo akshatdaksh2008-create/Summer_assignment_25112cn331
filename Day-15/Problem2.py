fdb447f9fd10efe059a79a4c8085d1476f6362e3
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:25:29 +0530

    Update Problem2.py

--- Day-15/Problem2.py
+++ Day-15/Problem2.py
@@ -1,1 +1,12 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+first = arr[0]
+for i in range(n - 1):
+    arr[i] = arr[i + 1]
+arr[n - 1] = first
+print("Array after left rotation:")
+for i in arr:
+    print(i, end=" ")

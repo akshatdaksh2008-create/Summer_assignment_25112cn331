9d8bdcd141a943c35c46f7c2e83d8d0b07a8607f
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:24:18 +0530

    Update problem1.py

--- Day-15/problem1.py
+++ Day-15/problem1.py
@@ -1,1 +1,8 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+print("Reversed array:")
+for i in range(n - 1, -1, -1):
+    print(arr[i], end=" ")

88be5aabb238b5b654c7afe1034b4a9d9c1bac5f
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:45:37 +0530

    Update problem4.py

--- Day-17/problem4.py
+++ Day-17/problem4.py
@@ -1,1 +1,14 @@
-
+arr1 = []
+arr2 = []
+n1 = int(input("Enter number of elements in first array: "))
+for i in range(n1):
+    num = int(input("Enter element: "))
+    arr1.append(num)
+n2 = int(input("Enter number of elements in second array: "))
+for i in range(n2):
+    num = int(input("Enter element: "))
+    arr2.append(num)
+print("Common elements are:")
+for i in arr1:
+    if i in arr2:
+        print(i)

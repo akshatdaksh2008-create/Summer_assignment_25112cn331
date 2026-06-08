f4dc9d488cf78c046b007160229af4cd46dae4ba
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:16:47 +0530

    Update problem1.py

--- Day-14/problem1.py
+++ Day-14/problem1.py
@@ -1,1 +1,14 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+key = int(input("Enter element to search: "))
+found = False
+for i in range(n):
+    if arr[i] == key:
+        print("Element found at position", i + 1)
+        found = True
+        break
+if found == False:
+    print("Element not found")

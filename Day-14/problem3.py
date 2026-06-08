71ed03c4eaf8b96dde1a545e7d05688cd73eaf60
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:21:51 +0530

    Update problem3.py

--- Day-14/problem3.py
+++ Day-14/problem3.py
@@ -1,1 +1,8 @@
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+arr.sort()
 
+print("Second Largest Element =", arr[-2])

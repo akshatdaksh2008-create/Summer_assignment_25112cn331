57f8abe3f390bb3a6229f25499e91da8fbdddc1f
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:32:44 +0530

    Update problem2.py

--- Day-16/problem2.py
+++ Day-16/problem2.py
@@ -1,1 +1,17 @@
-
+n = int(input("Enter size of array: "))
+arr = []
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+max_count = 0
+max_element = arr[0]
+for i in arr:
+    count = 0
+    for j in arr:
+        if i == j:
+            count += 1
+    if count > max_count:
+        max_count = count
+        max_element = i
+print("Element with maximum frequency =", max_element)
+print("Frequency =", max_count)

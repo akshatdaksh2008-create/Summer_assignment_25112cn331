412803f44c62e16141b163b6138f1eafb6ab6171
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:08:18 +0530

    Update problem1.py

--- Day-13/problem1.py
+++ Day-13/problem1.py
@@ -1,1 +1,8 @@
-
+n = int(input("Enter the size of array: "))
+arr = []
+for i in range(n):
+    x = int(input("Enter element: "))
+    arr.append(x)
+print("Array elements are:")
+for i in arr:
+    print(i, end=" ")

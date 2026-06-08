630096095a7698d78c7c5eb81bde2f25fb3f509b
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:49:23 +0530

    Update problem3.py

--- Day-18/problem3.py
+++ Day-18/problem3.py
@@ -1,1 +1,23 @@
+arr = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+arr.sort()   # Sort the array
+key = int(input("Enter element to search: "))
+low = 0
+high = n - 1
+found = False
+while low <= high:
+    mid = (low + high) // 2
+    if arr[mid] == key:
+        print("Element found at index", mid)
+        found = True
+        break
+    elif arr[mid] < key:
+        low = mid + 1
+    else:
+        high = mid - 1
 
+if found == False:
+    print("Element not found")

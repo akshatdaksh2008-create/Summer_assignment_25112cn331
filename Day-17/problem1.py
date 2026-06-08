16207d4b086420a600fb7c4a10295128a9c6e903
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:42:41 +0530

    Update problem1.py

--- Day-17/problem1.py
+++ Day-17/problem1.py
@@ -1,1 +1,13 @@
-
+arr1 = []
+arr2 = []
+n1 = int(input("Enter number of elements in first array: "))
+for i in range(n1):
+    num = int(input("Enter element: "))
+    arr1.append(num)
+n2 = int(input("Enter number of elements in second array: "))
+for i in range(n2):
+    num = int(input("Enter element: "))
+    arr2.append(num)
+merged_array = arr1 + arr2
+print("Merged Array:")
+print(merged_array)

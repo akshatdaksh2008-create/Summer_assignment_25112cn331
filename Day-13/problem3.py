9164457f07ae57d57bd226e7839f278a02912193
akshatdaksh2008-create <akshatdaksh2008@gmail.com>
2026-06-08 08:11:17 +0530

    Update problem3.py

--- Day-13/problem3.py
+++ Day-13/problem3.py
@@ -1,1 +1,15 @@
+n = int(input("Enter size of array: "))
+arr = []
 
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+largest = arr[0]
+smallest = arr[0]
+for i in arr:
+    if i > largest:
+        largest = i
+    if i < smallest:
+        smallest = i
+print("Largest element =", largest)
+print("Smallest element =", smallest)

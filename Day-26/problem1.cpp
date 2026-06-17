209a206904b170e2a32200ef5cc53a8bda948343
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:47:59 +0530

    Update problem1.cpp

--- Day-26/problem1.cpp
+++ Day-26/problem1.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    int number, guess;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    cout << "Guess the number (1 to 100):\n";
+    while(true)
+    {
+        cin >> guess;
+        if(guess > number)
+        {
+            cout << "Too high, try again: ";
+        }
+        else if(guess < number)
+        {
+            cout << "Too low, try again: ";
+        }
+        else
+        {
+            cout << "Correct! You guessed it.";
+            break;
+        }
+    }
 
+    return 0;
+}

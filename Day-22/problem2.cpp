f8198d9e9e71277b297da0e9443754698e7b2157
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:29:55 +0530

    Update problem2.cpp

--- Day-22/problem2.cpp
+++ Day-22/problem2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[200];
+    int count = 0;
+    cout << "Enter a sentence: ";
+    cin.getline(str, 200);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+        {
+            count++;
+        }
+    }
+    if(str[0] != '\0')
+    {
+        count = count + 1;
+    }
 
+    cout << "Number of words = " << count;
+
+    return 0;
+}

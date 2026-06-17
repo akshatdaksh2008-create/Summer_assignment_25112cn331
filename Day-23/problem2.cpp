d7df081fdf900799fda1021df296494e14f21740
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:35:01 +0530

    Update problem2.cpp

--- Day-23/problem2.cpp
+++ Day-23/problem2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[200];
+    int freq[256] = {0};
+   cout << "Enter a string: ";
+    cin.getline(str, 200);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        freq[(int)str[i]]++;
+    }
+    char result = '\0';
 
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(freq[(int)str[i]] > 1)
+        {
+            result = str[i];
+            break;
+        }
+    }
+    if(result != '\0')
+        cout << "First repeating character = " << result;
+    else
+        cout << "No repeating character found";
+
+    return 0;
+}

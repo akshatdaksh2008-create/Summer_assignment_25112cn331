2e5d02144fc1574c1f88a0d41cc1c0b842660f9e
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:28:27 +0530

    Update problem1.cpp

--- Day-22/problem1.cpp
+++ Day-22/problem1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    int length = 0;
+    bool isPalindrome = true;
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+    while(str[length] != '\0')
+    {
+        length++;
+    }
+    for(int i = 0; i < length / 2; i++)
+    {
+        if(str[i] != str[length - i - 1])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if(isPalindrome)
+        cout << "String is Palindrome";
+    else
+        cout << "String is Not Palindrome";
 
+    return 0;
+}

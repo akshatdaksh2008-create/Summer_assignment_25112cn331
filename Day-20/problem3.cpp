5f4e5576bd081564815085bc0d0f9eb626061b0e
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 18:52:36 +0530

    Update problem3.cpp

--- Day-20/problem3.cpp
+++ Day-20/problem3.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cols;
+    int a[10][10];
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << "Row-wise sums are:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        int sum = 0;
 
+        for(int j = 0; j < cols; j++)
+        {
+            sum = sum + a[i][j];
+        }
+
+        cout << "Sum of row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}

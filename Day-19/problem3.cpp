a256c187248ffd7990700bc1863ec861ca0b4f2b
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 18:39:55 +0530

    Update problem3.cpp

--- Day-19/problem3.cpp
+++ Day-19/problem3.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cols;
+    int matrix[10][10];
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "Transpose of matrix is:\n";
+    for(int i = 0; i < cols; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            cout << matrix[j][i] << " ";
+        }
+        cout << endl;
+    }
 
+    return 0;
+}

d9870ef2d0a23978b061fea8c565562d9e1a90a9
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:39:12 +0530

    Update problem1.cpp

--- Day-24/problem1.cpp
+++ Day-24/problem1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main()
+{
+    char str1[100], str2[100], temp[200];
+
+    cin.getline(str1, 100);
+    cin.getline(str2, 100);
+
+    if(strlen(str1) != strlen(str2))
+    {
+        cout << "Not Rotation";
+        return 0;
+    }
+
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    if(strstr(temp, str2) != NULL)
+        cout << "Rotation";
+    else
+        cout << "Not Rotation";
+
+    return 0;
+}

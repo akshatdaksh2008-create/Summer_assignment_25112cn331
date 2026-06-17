e7daa6da03b4a1228d0b1fda92c72ff8c4ad7281
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 18:27:31 +0530

    Update problem1.cpp

--- Day-19/problem1.cpp
+++ Day-19/problem1.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    int a[10][10], b[10][10], sum[10][10];
+    cout << "Enter elements of first matrix:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << "Enter elements of second matrix:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    cout << "Sum of matrices is:\n";
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
 
+    return 0;
+}

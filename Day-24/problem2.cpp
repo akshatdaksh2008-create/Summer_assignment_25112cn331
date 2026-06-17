a7945671e5a055e429076b1498cf7421906934df
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:40:18 +0530

    Update problem2.cpp

--- Day-24/problem2.cpp
+++ Day-24/problem2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[200];
+        cout << "Enter a string: ";
+    cin.getline(str, 200);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        int count = 1;
 
+        while(str[i] == str[i + 1])
+        {
+            count++;
+            i++;
+        }
+
+        cout << str[i] << count;
+    }
+
+    return 0;
+}

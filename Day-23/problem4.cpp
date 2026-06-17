31f23232680a0ab8764c5a4ef9c0fb9eb58fa494
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:37:46 +0530

    Update problem4.cpp

--- Day-23/problem4.cpp
+++ Day-23/problem4.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[200];
+    int freq[256] = {0};
 
+    cout << "Enter a string: ";
+    cin.getline(str, 200);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        freq[(int)str[i]]++;
+    }
+
+    char maxChar = str[0];
+    int maxCount = 0;
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(freq[(int)str[i]] > maxCount)
+        {
+            maxCount = freq[(int)str[i]];
+            maxChar = str[i];
+        }
+    }
+
+    cout << "Maximum occurring character = " << maxChar << endl;
+    cout << "Frequency = " << maxCount;
+
+    return 0;
+}

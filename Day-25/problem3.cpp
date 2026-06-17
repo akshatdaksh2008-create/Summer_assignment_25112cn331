9a48dc10f3d406d6006380dc0f6ee8c40145a9ac
Akshat Daksh <akshatdaksh2008@gmail.com>
2026-06-17 19:45:51 +0530

    Update problem3.cpp

--- Day-25/problem3.cpp
+++ Day-25/problem3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char name[50][50], temp[50];
+    int n;
+    cout << "Enter number of names: ";
+    cin >> n;
+    cin.ignore();
+    cout << "Enter names:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin.getline(name[i], 50);
+    }
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(strcmp(name[i], name[j]) > 0)
+            {
+                strcpy(temp, name[i]);
+                strcpy(name[i], name[j]);
+                strcpy(name[j], temp);
+            }
+        }
+    }
 
+    cout << "Sorted names:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << name[i] << endl;
+    }
+
+    return 0;
+}
